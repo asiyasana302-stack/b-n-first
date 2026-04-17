e45a446a246c76045e99a5c5affa7f63f7e68a00
asiyasana302-stack <asiyasana302@gmail.com>
2026-04-17 23:28:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
+<html>
+<head>
+  <title>JNTUH R25 Notes</title>
+</head>
+<body>
+<head>
+  <meta name="google-site-verification" content="N-TqnvNFVMi0_7dQ" />
+  <title>JNTUH R25 Notes</title>
+</head>
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
