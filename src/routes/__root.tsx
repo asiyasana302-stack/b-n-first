70d746f12eaf02382494695addfab7326036bc77
asiyasana302-stack <asiyasana302@gmail.com>
2026-04-18 05:47:04 +0530

    Added Google site verification meta tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,36 +1,5 @@
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
-
-
-import '../styles.css'
-
-export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'JNTUH R25 Notes',
-      },
-    ],
-  }),
-  shellComponent: RootDocument,
-})
-
-function RootDocument({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-      </head>
-      <body className="min-h-screen">
-        <main>{children}</main>
-        <Scripts />
-      </body>
-    </html>
-  )
-}
+const meta = [
+  ...,
+  { name: 'google-site-verification', content: 'N--TqnvNFVMi0_7dQCdhGSiCzMT1gQLAKSoQfLkOo3o' },
+  ...
+];
